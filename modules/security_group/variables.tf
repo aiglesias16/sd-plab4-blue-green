2ba728a37a7e7c71c7450aeacaf2d56eda28b44b
1665571 <1665571@uab.cat>
2025-12-01 19:13:36 +0100

    module/sg:1.0.0

---
+++ modules/security_group/variables.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  description = "Nombre del Security Group."
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID de la VPC donde se creará el Security Group."
+  type        = string
+}
+
+variable "ingress" {
+  description = "Reglas de entrada (ingress) para el Security Group."
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
+  default = []
+}
+
+variable "egress" {
+  description = "Reglas de salida (egress) para el Security Group."
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
+  default = []
+}
+
+variable "tags" {
+  description = "Tags adicionales para el Security Group."
+  type        = map(string)
+  default     = {}
+}
